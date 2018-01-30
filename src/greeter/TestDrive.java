fa174ff5198df2f0e994724abfb1d6c2a7245f6e
Sonswela <sonswela@yahoo.com>
2018-01-30 10:37:44 -0600

    Created and tested Greeter Class

---
+++ src/greeter/TestDrive.java
@@ -0,0 +1,12 @@
+package greeter;
+
+public class TestDrive {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeter greet = new Greeter("Dingo", 25);
+		System.out.println(greet.toString());
+			
+	}
+
+}
